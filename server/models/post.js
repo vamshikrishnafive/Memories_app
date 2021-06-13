e4064b8b1fa1cf5ab059a8bb9052e8ce7b5ef792
vamshikrishnafive <saikrishana9@gmail.com>
2021-06-13 22:59:02 +0530

    post model updated

--- server/models/post.js
+++ server/models/post.js
@@ -3,12 +3,9 @@ import mongoose from 'mongoose';
 const PostSchema = mongoose.Schema({
     title:String,
     message:String,
-    tags:String,
+    tags:[String],
     selectedFile:String,
-    likeCount: {
-        type: Number,
-        default:0
-    },
+    likeCount: { type:[String],default:[]},
     createdAt: {
         type: Date,
         default:new Date
