83105467c66e633849196adfc728f43830294a89
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-10-05 11:05:21 +0530

    2

--- server/routers/post.js
+++ server/routers/post.js
@@ -1,16 +1,15 @@
 import express from 'express';
 
-import { createPost, deletePost, getPosts, getSinglePost, likePost, updatePost } from '../controllers/post';
+import { createPost, deletePost, getPosts, getPost, likePost, updatePost } from '../controllers/post';
 import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get('/', getPosts)
-router.get('/:id', getSinglePost)
-
-router.post('/', auth, createPost)
-router.patch('/:id', auth, updatePost)
-router.patch('/:id/likePost', auth, likePost)
-router.delete('/:id', auth, deletePost)
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
