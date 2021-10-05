4bdc526ce511d1dddc0ea20de856b67c3a429bad
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-10-05 11:11:29 +0530

    31

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import './index.css'
-import { reducers } from './reduces'
+import { reducers } from './reduces';
 import App from './App';
+import './index.css';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <App />
     </Provider>,
     document.getElementById('root'),
 );
-
